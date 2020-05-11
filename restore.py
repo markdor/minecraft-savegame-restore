9571af42e2fcdc86770b28981014527c4924b112
Markus Doerfler <markus.doerfler@gmail.com>
2020-05-11 17:15:24 +0200

    linting

--- restore.py
+++ restore.py
@@ -2,13 +2,15 @@ import sys
 from appJar import gui
 
 # get path for resources
-if getattr(sys, 'frozen', False): # Running as compiled
-    running_dir = sys._MEIPASS + "/" # Same path name than pyinstaller option
+if getattr(sys, 'frozen', False):
+    # pylint: disable=no-member
+    running_dir = sys._MEIPASS + "/"
 else:
-    running_dir = "./" # Path name when run with Python interpreter
+    running_dir = "./"
 
 # create a GUI variable called app
 app = gui()
+app.setSize("600x400")
 app.setIcon(running_dir + "creeper.ico")
 app.setTitle("Minecraft Savegame Restore")
 app.addLabel("title", "asd")
