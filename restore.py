93f89280c6d93b4bccd54e144c016e684c091d87
Markus Doerfler <markus.doerfler@gmail.com>
2020-05-12 15:08:46 +0200

    showing messages

--- restore.py
+++ restore.py
@@ -3,6 +3,15 @@ import glob
 import os
 from appJar import gui
 
+def press(button):
+    if button == "Cancel":
+        app.stop()
+    else:
+        restore()
+
+def restore():
+    app.setMessage("logger", app.getMessage("logger") + "\nhuhu")
+
 # get path for resources
 if getattr(sys, 'frozen', False):
     # pylint: disable=no-member
@@ -12,16 +21,22 @@ else:
 
 # get latest backup
 minecraft_backup_dir = os.path.expandvars(r'%APPDATA%\.minecraft\backups\*')
-files = glob.glob(minecraft_backup_dir)
-minecraft_backup_filepath = max(files, key=os.path.getctime)
+minecraft_save_dir = os.path.expandvars(r'%APPDATA%\.minecraft\saves')
+minecraft_backup_filepath = max(glob.glob(minecraft_backup_dir), key=os.path.getctime)
 minecraft_backup_filename = os.path.basename(minecraft_backup_filepath)
 
 # create a GUI variable called app
-app = gui("Minecraft Savegame Restore", "400x200")
+app = gui("Minecraft Savegame Restore", "600x100")
 app.setIcon(running_dir + "creeper.ico")
+
 app.addLabelEntry("Backup-File")
 app.setEntry("Backup-File", minecraft_backup_filename)
-app.getLabelWidget("Backup-File")
+app.setEntryState("Backup-File", "disabled")
+
+app.addButtons(["Ok", "Cancel"], press)
+
+app.addMessage("logger", "starting up")
+app.setMessageWidth("logger", 600)
 
 # start the GUI
 app.go()
