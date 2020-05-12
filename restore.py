9829d6b7b221dca30bd4823f7abd31267a93251d
Markus Doerfler <markus.doerfler@gmail.com>
2020-05-12 12:07:37 +0200

    get newest backupfile

--- restore.py
+++ restore.py
@@ -1,4 +1,6 @@
 import sys
+import glob
+import os
 from appJar import gui
 
 # get path for resources
@@ -8,12 +10,18 @@ if getattr(sys, 'frozen', False):
 else:
     running_dir = "./"
 
+# get latest backup
+minecraft_backup_dir = os.path.expandvars(r'%APPDATA%\.minecraft\backups\*')
+files = glob.glob(minecraft_backup_dir)
+minecraft_backup_filepath = max(files, key=os.path.getctime)
+minecraft_backup_filename = os.path.basename(minecraft_backup_filepath)
+
 # create a GUI variable called app
-app = gui()
-app.setSize("600x400")
+app = gui("Minecraft Savegame Restore", "400x200")
 app.setIcon(running_dir + "creeper.ico")
-app.setTitle("Minecraft Savegame Restore")
-app.addLabel("title", "asd")
+app.addLabelEntry("Backup-File")
+app.setEntry("Backup-File", minecraft_backup_filename)
+app.getLabelWidget("Backup-File")
 
 # start the GUI
 app.go()
